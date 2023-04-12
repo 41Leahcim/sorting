e197c83e408562181101de849f9b33caf876aebc
Michael Scholten <michael-scholten@hotmail.nl>
2023-04-12 10:22:09 +0200

    Performance is now measured with perf_counter instead of time.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from time import time
+from time import perf_counter
 from merge_sort import merge_sort
 from insertion_sort import insertion_sort
 from bubble_sort import bubble_sort
@@ -7,11 +7,11 @@ from bogo_sort import bogo_sort
 
 def main():
     algorithm = bogo_sort
-    start = time()
+    start = perf_counter()
     values = [i for i in range(16, 0, -1)]
     algorithm(values)
     print(values)
-    print(time() - start)
+    print(perf_counter() - start)
 
 if __name__ == "__main__":
     main()
